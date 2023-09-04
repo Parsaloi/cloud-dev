804fcbb0d85ed839e3058dc7862854740d14413a
Parsaloi <parsaloi35@gmail.com>
2023-09-05 01:54:19 +0300

    Writing Web Application in Clojure to deploy as Cloud Web Services:)...

---
+++ clojure-dev/web_services_examples/simple_web_server/using_ring_to_handle_http_requests_http_responses/ring-app/src/ring_app/core.clj
@@ -0,0 +1,26 @@
+(ns ring-app.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
+
+(defn handler [request-map]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "<html><body> Yello from creatorp, my IP is: "
+              (:remote-addr request-map)
+              "</body></html>")})
+
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+
+(defn -main []
+  (jetty/run-jetty
+    (-> handler
+        var
+        wrap-nocache
+        wrap-reload)
+        {:port 3002
+         :join? false}))
+
